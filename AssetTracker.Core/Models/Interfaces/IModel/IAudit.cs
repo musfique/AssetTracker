22ffdce77f3005f8534ded9494e4f34110ccdee1
Sk. Musfique Ahmed <musfique4u@yahoo.com>
2017-03-03 11:22:59 +0600

    Added audit for future use

---
+++ AssetTracker.Core/Models/Interfaces/IModel/IAudit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssetTracker.Core.Models.Interfaces.IModel
+{
+    public interface IAudit
+    {
+        string CreatedBy { get; set; }
+        DateTime CreatedOn { get; set; }
+        string LastModifiedBy { get; set; }
+        DateTime? LastModifiedOn { get; set; }
+    }
+}
